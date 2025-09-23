9b456d21990b7b37f799ec3254df591d447fbd53
anaswara11111-lang <anaswara11111@gmail.com>
2025-09-23 16:20:33 +0530

    Add files via upload

---
+++ 23-09-2025/color.py
@@ -0,0 +1,3 @@
+colors=input("Enter colors seperated by commas:").split(",")
+print("First color:",colors[0].strip())
+print("Last color:",colors[-1].strip())
---
+++ 23-09-2025/colorlist.py
@@ -0,0 +1,6 @@
+color_list1 = ['red', 'blue', 'green', 'yellow']
+color_list2 = ['blue', 'yellow', 'black']
+
+for color in color_list1:
+    if color not in color_list2:
+        print(color)
